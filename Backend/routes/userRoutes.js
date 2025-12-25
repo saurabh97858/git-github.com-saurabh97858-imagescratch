3a19c9eb230dcb77ea5765cfe149c2c1de065408
saurabh97858 <saurabhgupta97858@gmail.com>
2025-12-25 15:15:47 +0530

    Fix: Replace broken clerkAuth

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { userCredits, paymentRazorpay, verifyRazorpay } from "../controllers/userController.js";
 import { resetCredits } from "../controllers/creditController.js";
-import clerkAuth from "../middlewares/clerkAuth.js";
+import userAuth from "../middlewares/auth.js";
 
 const userRouter = express.Router();
 
 // 🆕 Protected Routes using Clerk
-userRouter.get("/credits", clerkAuth, userCredits);
-userRouter.post("/pay-razor", clerkAuth, paymentRazorpay);
-userRouter.post("/reset-credits", clerkAuth, resetCredits); // Reset credits to 100
+userRouter.get("/credits", userAuth, userCredits);
+userRouter.post("/pay-razor", userAuth, paymentRazorpay);
+userRouter.post("/reset-credits", userAuth, resetCredits); // Reset credits to 100
 
 // Dummy verify route
 userRouter.post("/verify-razor", verifyRazorpay);
